20091160697c9b8e4b9aed97aa80955b7c6a491f
Derek Truong <derektruongdev@gmail.com>
2022-10-23 13:40:23 -0500

    initial files

---
+++ README.md
@@ -0,0 +1,21 @@
+# CSV to JSON converter
+A program that accepts CSV file to JSON structure.
+
+**Link to project:** https://###
+
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript
+
+
+## Lessons Learned:
+
+CSV file syntax.
+
+JSON file syntax.
+
+Flexbox layout.
+
+Imrpove readability. Break down large functions using helper functions.
+
---
+++ main.js
@@ -0,0 +1,53 @@
+let csv = `"Day of Week", "Number of Crashes"
+"Sunday", 13664
+"Monday", 17279
+"Tuesday", 17337
+"Wednesday", 17394
+"Thursday", 17954
+"Friday", 19147
+"Saturday", 15714
+ `
+
+//console.log(csv.split('\n'));
+
+/*
+- get keys
+- iterate through csv make line string into array
+
+*/
+
+
+csv = csv.trim().split('\n');
+
+function formatValues(string) {
+  return string.split(',').map(key => key.trim());
+}
+
+function buildObject(keys, values) {
+  let currentObj = {};
+  for (let j = 0; j < keys.length; j++) {
+    currentObj[keys[j]] = values[j]
+  }
+  return currentObj;
+}
+
+function getKeys(csv) {
+  return formatValues(csv[0]);
+}
+
+function getEntries(csv) {
+  let result = [];
+  let keys = getKeys(csv);
+  for (let i = 1; i < csv.length; i++) {
+    let line = csv[i];
+    let values = formatValues(line);
+    let object = buildObject(keys, values);
+    result.push(object);
+  }
+  return result;
+}
+
+console.log(getEntries(csv));
+
+
+//console.dir(keys)
